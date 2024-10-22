32e37ce86ecd2f205db75307ac539e4f825df461
Agus Setiawan <agussetiawan.me02@gmail.com>
2024-10-22 22:12:29 +0700

    verison 0.3.2

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -130,7 +130,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Demografi Jangkauan DKI Jakarta</h6>
+                                        <h6 class="text-muted font-semibold">Total Viewers ( Area Jakarta )</h6>
                                         <h6 class="font-extrabold mb-0" id="total-semua-kota"></h6>
                                     </div>
                                 </div>
@@ -147,7 +147,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Reach/Viewers</h6>
+                                        <h6 class="text-muted font-semibold">Total Akun Dijangkau ( Area Jakarta )</h6>
                                         <h6 class="font-extrabold mb-0" id="total-reach"></h6>
                                     </div>
                                 </div>
@@ -277,6 +277,43 @@
                     </div>
                 </div>
 
+
+                <!-- <div class="row">
+                    <div class="col-12 col-lg-3 col-md-12">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                        <div class="stats-icon purple mb-2">
+                                            <i class="fas fa-play"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">Total Viewers ( Area Jakarta )</h6>
+                                        <h6 class="font-extrabold mb-0" id="total-semua-kota"></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-md-12">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                        <div class="stats-icon blue mb-2">
+                                            <i class="fas fa-eye"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">Total Akun Dijangkau ( Area Jakarta )</h6>
+                                        <h6 class="font-extrabold mb-0" id="total-reach"></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
 
                 <div class="row">
                     <!-- Profile Visit -->
