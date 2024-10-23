0b1e4728be17c22c9d48462b779484ae4b0460aa
Agus Setiawan <agussetiawan.me02@gmail.com>
2024-10-23 20:24:35 +0700

    0.3.4

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -120,7 +120,7 @@
                 <!-- <h4 id="date-output" class="font-extrabold text-center my-3"></h4> -->
 
                 <div class="row">
-                    <div class="col-6 col-lg-3 col-md-6">
+                    <div class="col-6 col-lg-4 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
@@ -137,7 +137,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
+                    <div class="col-6 col-lg-4 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
@@ -154,7 +154,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
+                    <div class="col-12 col-lg-4 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
@@ -166,23 +166,6 @@
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Like</h6>
                                         <h6 class="font-extrabold mb-0" id="total-like"></h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon red mb-2">
-                                            <i class="fas fa-user"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Kunjungan Profil</h6>
-                                        <h6 class="font-extrabold mb-0" id="total-kunjungan"></h6>
                                     </div>
                                 </div>
                             </div>
@@ -279,16 +262,36 @@
 
 
                 <div class="row">
-                    <div class="col-6">
+                    <!-- Kolom Kunjungan Profil -->
+                    <div class="col-6 col-lg-4 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
-                                    <div class="col-6 d-flex justify-content-start">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                        <div class="stats-icon red mb-2">
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">Kunjungan Profil</h6>
+                                        <h6 class="font-extrabold mb-0" id="total-kunjungan"></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Kolom Biaya Per Viewers -->
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                                         <div class="stats-icon purple mb-2">
                                             <i class="fas fa-play"></i>
                                         </div>
                                     </div>
-                                    <div class="col-6">
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Biaya Per Viewers</h6>
                                         <h6 class="font-extrabold mb-0"> Rp {{ number_format($budget->biaya_perviewers, 0, ',', '.') }}</h6>
                                     </div>
@@ -296,16 +299,18 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
+
+                    <!-- Kolom Total Dibelanjakan -->
+                    <div class="col-12 col-lg-4 col-md-6">
                         <div class="card">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
-                                    <div class="col-6 d-flex justify-content-start">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                                         <div class="stats-icon blue mb-2">
                                             <i class="fas fa-eye"></i>
                                         </div>
                                     </div>
-                                    <div class="col-6">
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Total Dibelanjakan</h6>
                                         <h6 class="font-extrabold mb-0">Rp {{ number_format($budget->total_dibelanjakan, 0, ',', '.') }}</h6>
                                     </div>
@@ -314,6 +319,7 @@
                         </div>
                     </div>
                 </div>
+
 
 
                 <div class="row">
@@ -391,7 +397,6 @@
 
     <script src="{{asset('assets/extensions/dayjs/dayjs.min.js')}}"></script>
     <script src="{{asset('assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
-    <script src="{{asset('assets/static/js/pages/dashboard.js')}}"></script>
 
     <script>
         // Menampilkan tanggal sekarang
